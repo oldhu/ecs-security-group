87a49cccab1b5f696076265c452ef183926ebdea
oldhu <me@oldhu.com>
2015-02-15 22:58:29 +0800

    title fix

--- editsg.js
+++ editsg.js
@@ -3,12 +3,10 @@ var remote = require('remote');
 function load_sg_values() {
     var sg_to_edit = remote.getGlobal('sharedObject').sg;
     if (sg_to_edit == null) {
-        $('#sgid').val("");
         $('#regionid').val(getRegion());
         $('#sgname').val("");
         $('#sgname').focus();
     } else {
-        $('#sgid').val(sg_to_edit.sgid);
         $('#sgname').val(sg_to_edit.sgname);
     }
 }
